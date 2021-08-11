8cdc0682aea335059f2c7f51f7c890c2e8baa615
olegaposh <oleg.poshiv@gmail.com>
2021-08-10 18:16:15 -0700

    finiched Joke App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,72 @@
+import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Axios from 'axios';
+import { react } from '@babel/types';
+import Header from './components/header'
 
 function App() {
+
+  const [joke, setJoke] = useState("");
+  const [answer, setAnswer] = useState("");
+
+  const getJoke = () => {
+
+    Axios.get("https://v2.jokeapi.dev/joke/Any").then(
+      (response) => {
+        console.log(response);
+        // setJoke(response.data.setup + " ..." + response.data.delivery);
+        setJoke(response.data.setup);
+        setAnswer(response.data.delivery);
+        console.log(response.data.setup);
+        console.log(response.data.delivery);
+      }
+    )
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+      <Header />
+      <div className="App">
+        <div className="container">
+          <div className="row">
+            <div className="col my-5">
+
+            </div>
+            <div className="col-lg-10 my-5">
+              <button type="button" className="btn btn-success btn-lg" onClick={getJoke}>Tell me a Joke</button>
+            </div>
+            <div className="col">
+
+            </div>
+          </div>
+          <div className="row">
+            <div className="col my-5">
+
+            </div>
+            <div className="joke col-lg-10 my-5 py-5">
+             <p>{joke}</p> 
+
+            </div>
+            <div className="col">
+
+            </div>
+          </div>
+          <div className="row">
+    <div className="col my-5">
+      
     </div>
+    <div className="answer col-lg-10 my-5 py-5">
+      <p>{answer}</p>
+      
+    </div>
+    <div className="col">
+      
+    </div>
+  </div>
+        </div>
+      </div>
+    </>
   );
 }
 
---
+++ src/components/header.js
@@ -0,0 +1,18 @@
+import '../components/header.css';
+
+
+const Header = () => {
+
+    return (
+        <div className="container-fluid">
+            <div className="row p-5 bg-primary">
+                <div className="col text-center">
+                    <h1>Jokes</h1>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Header;
